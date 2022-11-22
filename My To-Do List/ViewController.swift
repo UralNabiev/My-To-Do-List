38a10b8032660ec14a3bc6bdf928523d4dce98ee
Ural Nabiev <devuralnabiev@gmail.com>
2022-11-22 10:49:05 +0500

    Initial Commit

---
+++ My To-Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My To-Do List
+//
+//  Created by O'ral Nabiyev on 22/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
